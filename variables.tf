f6b50caaa731d0afe7cfb67696ab37b0affb5555
danielev04 <daniele@vottero.eu>
2019-02-19 16:09:47 +0000

    `traffic_manager_endpoints` are now passed as a map parameter

--- variables.tf
+++ variables.tf
@@ -1,27 +1,6 @@
-variable "shutter_page_target" {
-  type        = "string"
-  description = "Shutter page target address"
-}
-
 variable "org_name" {
   type        = "string"
   description = "Organization name"
-}
-
-variable "backend_name" {
-  type        = "string"
-  description = "Name of the backend"
-}
-
-variable "backend_pip" {
-  type        = "string"
-  description = "Public ip of the backend"
-}
-
-
-variable "public_hostname" {
-  type = "string"
-  description = "The public hostanme to use when accessing app frontend"
 }
 
 variable "product" {
@@ -44,3 +23,6 @@ variable "resource_group" {
   description = "Name of the resource group"
 }
 
+variable "traffic_manager_endpoints" {
+  type = "map"
+}
